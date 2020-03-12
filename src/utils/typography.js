3b8d900115a5178b1de993bdb3273225b83b0d3f
Nacxit <tierrasalvaje_02@hotmail.com>
2020-03-12 12:11:43 -0600

    add yarn package and move archive and add typography plugins

---
+++ src/utils/typography.js
@@ -0,0 +1,16 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    "Arial",
+    "Avenir Next",
+    "Helvetica Neue",
+    "Helvetica",
+    "sans-serif",
+    "Segoe UI",
+  ],
+  bodyFontFamily: ["Fjalla One", "Source Sans Pro", "Funston"],
+})
+export default typography
