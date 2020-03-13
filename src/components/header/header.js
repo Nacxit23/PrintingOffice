37120a1c6e666f00017193a5950d9ad21ed7b9c3
Nacxit <tierrasalvaje_02@hotmail.com>
2020-03-12 21:16:42 -0600

    update header link and style link

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 import { graphql, useStaticQuery, Link } from "gatsby"
 import Img from "gatsby-image"
-import { ElementLink } from "../styles/component"
 
 export default () => {
   const data = useStaticQuery(graphql`
@@ -23,22 +22,21 @@ export default () => {
     <header>
       <nav className="navbar navbar-expand-lg navbar-light headerColor">
         <ul className="navbar-nav mr-auto">
-          <Link className="navbar-brand colorText" to="/#">
+          <Link className="navbar-brand color-link" to="/#">
             <Img className="sizeImg" fluid={data.file.childImageSharp.fluid} />
           </Link>
+          <Link className="nav-link color-link" to="/#">
+            Home
+          </Link>
+          <Link className="nav-link color-link" to="/#">
+            Sobre nosotros
+          </Link>
+          <Link className="nav-link color-link" to="/#">
+            Servicios
+          </Link>
 
-          <Link className="nav-link colorText" to="/#">
-            <ElementLink>Home</ElementLink>
-          </Link>
-          <Link className="nav-link colorText" to="/#">
-            <ElementLink>Sobre nosotros</ElementLink>
-          </Link>
-          <Link className="nav-link colorText" to="/#">
-            <ElementLink>Servicios</ElementLink>
-          </Link>
-
-          <Link className="nav-link colorText" to="/#">
-            <ElementLink>Contactanos</ElementLink>
+          <Link className="nav-link color-link" to="/#">
+            Contactanos
           </Link>
         </ul>
       </nav>
