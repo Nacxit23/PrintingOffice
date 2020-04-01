5b3d81d7ae5a176789758636ef9f935c102e5102
Nacxit <tierrasalvaje_02@hotmail.com>
2020-04-01 12:12:26 -0600

    create index  links components

---
+++ src/components/browserComponent.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./header/header"
+export { default as Footer } from "./footer/footer"
+export { default as GalleryProduct } from "./GalleryProduct/GalleryProduct"
+export { default as CardInfo } from "./cardInfo/cardInfo"
