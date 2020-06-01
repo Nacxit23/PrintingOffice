766f35eb8be1e2a95dacc587c3b24b902622a6cc
Nacxit <nacxitArmando@hotmail.com>
2020-06-01 12:52:23 -0600

    Create AboutUs view

---
+++ src/pages/AboutUs/aboutUs.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {Layout} from "../../components/browserComponent";
+
+export default () => {
+  return (
+    <Layout>
+      <h1>Test</h1>
+    </Layout>
+  );
+}
