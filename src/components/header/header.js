6b6e981c3a8461c55a0d5b379e625915416db9a1
Nacxit <nacxitArmando@hotmail.com>
2020-06-05 19:38:39 -0600

    Create link service en add Anilink

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,6 @@ import { graphql, useStaticQuery, Link } from "gatsby"
 import Img from "gatsby-image"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
-
 export default () => {
   const data = useStaticQuery(graphql`
     query Logo {
@@ -23,19 +22,46 @@ export default () => {
     <header>
       <nav className="navbar navbar-expand-lg navbar-light headerColor">
         <ul className="navbar-nav mr-auto">
-          <AniLink className="nav-link color-link" direction="right" paintDrip to="/" duration={1} hex="#8BBED3">
-            <Img className="sizeImg" fluid={data.file.childImageSharp.fluid}/>
+          <AniLink
+            className="nav-link color-link"
+            direction="right"
+            paintDrip
+            to="/"
+            duration={1}
+            hex="#8BBED3"
+          >
+            <Img className="sizeImg" fluid={data.file.childImageSharp.fluid} />
           </AniLink>
-          <AniLink className="nav-link color-link" direction="right" paintDrip to="/" duration={1} hex="#8BBED3">
+          <AniLink
+            className="nav-link color-link"
+            direction="right"
+            paintDrip
+            to="/"
+            duration={1}
+            hex="#8BBED3"
+          >
             Home
           </AniLink>
-          <AniLink className="nav-link color-link" direction="right" paintDrip to="/AboutUs/aboutUs" duration={1}
-                   hex="#8BBED3">
+          <AniLink
+            className="nav-link color-link"
+            direction="right"
+            paintDrip
+            to="/AboutUs/aboutUs"
+            duration={1}
+            hex="#8BBED3"
+          >
             Sobre nosotros
           </AniLink>
-          <Link className="nav-link color-link" to="/#">
+          <AniLink
+            className="nav-link color-link"
+            direction="right"
+            paintDrip
+            to="/Service/service"
+            duration={1}
+            hex="#8BBED3"
+          >
             Servicios
-          </Link>
+          </AniLink>
           <Link className="nav-link color-link" to="/#">
             Contactanos
           </Link>
