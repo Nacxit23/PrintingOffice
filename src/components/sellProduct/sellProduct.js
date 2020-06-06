f5377278c3d9cb5c1111f35fc15c5c4b512bbadf
Nacxit <nacxitArmando@hotmail.com>
2020-06-05 23:46:46 -0600

    update css

--- src/components/sellProduct/sellProduct.js
+++ src/components/sellProduct/sellProduct.js
@@ -3,13 +3,13 @@ import sellStyle from "./sellProduct.module.css"
 
 export default () => {
   return <div className={`row align-items-center ${sellStyle.box}`}>
-    <div className={`col-6 text-center`}>
+    <div className={`col text-center`}>
       <p className={`ml-5 ${sellStyle.letter}`}>Contactanos para
         solicitor detalles de
         estos productos</p>
     </div>
-    <div className="col-6 text-center">
-      <button className={`btn ${sellStyle.button}`}>Contactanos</button>
+    <div className="col text-center">
+      <button className={`mb-4 btn ${sellStyle.button}`}>Contactanos</button>
     </div>
   </div>
 }
